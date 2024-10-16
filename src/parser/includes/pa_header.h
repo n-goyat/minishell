50ac7b024dd790911e0dd92781ce447bcade278e
n-goyat <134531117+n-goyat@users.noreply.github.com>
2024-10-16 14:44:35 +0200

    reworked tokenizer

--- src/parser/includes/pa_header.h
+++ src/parser/includes/pa_header.h
@@ -6,7 +6,7 @@
 /*   By: ngoyat <ngoyat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 17:41:05 by ngoyat            #+#    #+#             */
-/*   Updated: 2024/10/14 18:09:16 by ngoyat           ###   ########.fr       */
+/*   Updated: 2024/10/16 14:41:06 by ngoyat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,56 +25,94 @@
 
 typedef struct s_env
 {
-	char			*key;
-	char			*value;
-	struct s_env	*next;
-}					t_env;
+	char				*key;
+	char				*value;
+	struct s_env		*next;
+}						t_env;
 
+// Tekonization structs
 typedef struct s_token
 {
-	char			*value;
-	int				type;
-	struct s_token	*next;
-}					t_token;
+	int					type;
+	char				*value;
+	struct s_token		*next;
+}						t_token;
 
 typedef enum e_token_type
 {
-	TOKEN_COMMAND,
-	TOKEN_ARGUMENT,
+	TOKEN_WORD,
 	TOKEN_PIPE,
 	TOKEN_REDIRECT_IN,
 	TOKEN_REDIRECT_OUT,
 	TOKEN_APPEND,
-	TOKEN_OTHER
-}					t_token_type;
+	TOKEN_HEREDOC,
+	TOKEN_SIN_QOTES,
+	TOKEN_DBL_QOTES,
+	TOKEN_OTHER,
+}						t_token_type;
 
-// Struct to hold the tokenization state
-typedef struct s_tokenizer_state
+// File structs
+typedef struct s_file_node
 {
-	char			*input;
-	char			buffer[1024];
-	int				buf_index;
-	int				i;
-	t_token			**token_list;
-}					t_tokenizer_state;
+	char				*input_file;
+	char				*output_file;
+	int append; // 1 for append (>>), 0 for overwrite (>)
+	char				*heredoc;
+	struct s_file_node	*next;
+}						t_file_node;
+
+typedef struct s_files_list
+{
+	t_file_node			*head;
+	t_file_node			*tail;
+	size_t				size;
+}						t_files_list;
+
+// Command structs
+typedef enum e_cmd_type
+{
+	CMD_SIMPLE,       // Simple command
+	CMD_PIPE,         // Piping (|)
+	CMD_AND,          // Logical AND (&&)
+	CMD_OR,           // Logical OR (||)
+	CMD_REDIRECT_IN,  // Redirect input (<)
+	CMD_REDIRECT_OUT, // Redirect output (>)
+	CMD_APPEND_OUT    // Append output (>>)
+}						t_cmd_type;
+
+typedef struct s_cmd_node
+{
+	int					type;
+	t_files_list		*files;
+	char				**cmd;
+	struct s_cmd_node	*next;
+}						t_cmd_node;
+
+typedef struct s_commands_list
+{
+	t_cmd_node			*head;
+	t_cmd_node			*tail;
+	size_t				size;
+}						t_commands_list;
 
 //	pa_main_env_creation
-t_env				*create_node(char *env_var);
-void				add_node(t_env **env_list, t_env *new_node);
-t_env				*init_env_list(char **env);
-void				print_env_list(t_env *env_list);
-void				free_env_list(t_env *env_list);
+t_env					*create_node(char *env_var);
+void					add_node(t_env **env_list, t_env *new_node);
+t_env					*init_env_list(char **env);
+void					print_env_list(t_env *env_list);
+void					free_env_list(t_env *env_list);
 
 //	pa_tokenizer
-t_token				*create_token(char *value, int type);
-void				add_token(t_token **token_list, t_token *new_token);
-int					determine_type(char *token);
-int					handle_quotes(t_tokenizer_state *state);
-void				handle_double_greater(t_tokenizer_state *state);
-int					handle_operator(t_tokenizer_state *state);
-int					add_buffer_to_tokens(t_tokenizer_state *state);
-int					process_input_char(t_tokenizer_state *state);
-t_token				*tokenize_input(char *input);
-void				print_tokens(t_token *tokens);
+t_token					*create_token(char *value, int type);
+void					add_token(t_token **token_list, t_token *new_token);
+int						determine_type(char *token);
+void					print_tokens(t_token *tokens);
+int						word_len(char *word);
+int						write_token(char *in, int *i, t_token *token,
+							t_token_type typ);
+int						handle_quotes(char *in, t_token *token,
+							t_token_type typ);
+int						assign_token_typ(char *in, int *i, t_token *token);
+t_token					*tokenize_input(char *in);
 
 #endif
