6e5122ec6b03eed7475fcf57b9547943c42b7ee4
Mamadou Cherif Ba <maba@2-B-9.42heilbronn.de>
2024-11-24 15:13:01 +0100

    j'avais creer une variable dans la fonction ft_wait_for_processes.c que j'avais pas utiliser du coup je l'ai juste supprimer

--- src/execution/ft_wait_for_processes.c
+++ src/execution/ft_wait_for_processes.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_wait_for_processes.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ngoyat <ngoyat@student.42.fr>              +#+  +:+       +#+        */
+/*   By: maba <maba@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 14:57:12 by maba              #+#    #+#             */
-/*   Updated: 2024/11/23 04:56:41 by ngoyat           ###   ########.fr       */
+/*   Updated: 2024/11/24 15:07:45 by maba             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 void	ft_wait_for_processes(pid_t pid, t_env_list *env_list)
 {
 	int	status;
-	int	signal;
 
 	waitpid(pid, &status, 0);
 	if (WIFEXITED(status))
@@ -25,8 +24,5 @@ void	ft_wait_for_processes(pid_t pid, t_env_list *env_list)
 		ft_exit(env_list, env_list->exit_code);
 	}
 	else if (WIFSIGNALED(status))
-	{
-		signal = WTERMSIG(status);
 		ft_putstr_fd("Process terminated by signal\n", STDERR_FILENO);
-	}
 }
